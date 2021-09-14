e1b237b77a33311b39fbb965fe60d8a0fa1b94d2
Arcadio Arevalo <arevaloarcadio.96@gmail.com>
2021-09-14 02:46:58 -0400

    configurado la bd

--- dist/database.js
+++ dist/database.js
@@ -3,11 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.pool = void 0;
 const pg_1 = require("pg");
 exports.pool = new pg_1.Pool({
-    user: 'postgres',
-    host: 'localhost',
-    password: 'postgres',
-    database: 'upgrap',
-    port: 5432
+    user: 'doadmin',
+    host: 'db-postgresql-nyc3-70537-do-user-9839254-0.b.db.ondigitalocean.com',
+    password: 'F7XOI8sLmOyH-PVT',
+    database: 'defaultdb',
+    port: 5432,
+    ssl: true
 });
 //WrQv%UO-0]i_ clave de usuario de cpanel
 // export const pool = new Pool({
--- src/database.ts
+++ src/database.ts
@@ -1,11 +1,12 @@
 import { Pool } from 'pg';
 
 export const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
-    password: 'postgres',
-    database: 'upgrap',
-    port: 5432
+    user: 'doadmin',
+    host: 'db-postgresql-nyc3-70537-do-user-9839254-0.b.db.ondigitalocean.com',
+    password: 'F7XOI8sLmOyH-PVT',
+    database: 'defaultdb',
+    port: 25060,
+    ssl : true
 });
 //WrQv%UO-0]i_ clave de usuario de cpanel
 // export const pool = new Pool({
