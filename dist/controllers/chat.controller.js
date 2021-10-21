ec4a85237b251f399b3ffede35c88eb183c56c1d
Arcadio Arevalo <arevaloarcadio.96@gmail.com>
2021-10-21 11:30:44 -0400

    mejoras chat

--- dist/controllers/chat.controller.js
+++ dist/controllers/chat.controller.js
@@ -36,8 +36,7 @@ const getChats = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
         for (var i = 0; i < chats.rows.length; i++) {
             const last_message = yield database_1.pool.query('SELECT * FROM messages WHERE WHERE ((id_sender = $1 AND id_receiver = $2) OR (id_sender = $2 AND id_receiver = $1)) and id_request = $3 ORDER BY fecha DESC LIMIT 1', [user_id, chats.rows[i].id_customer]);
             const customer = yield database_1.pool.query('SELECT name, email, city, dir, pais, phone, validate_phone, migrate_key, prefix, userid, singin_method, rol, id FROM customer WHERE id = $1 LIMIT 1', [chats.rows[i].id_customer]);
-            if ()
-                response.push(Object.assign(Object.assign({}, chats.rows[i]), { last_message: last_message.rows.length != 0 ? last_message.rows[0] : '', customer: customer.rows[0] }));
+            response.push(Object.assign(Object.assign({}, chats.rows[i]), { last_message: last_message.rows.length != 0 ? last_message.rows[0] : '', customer: customer.rows[0] }));
         }
         return res.status(200).json(response.sort(compare));
     }
